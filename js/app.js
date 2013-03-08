72d46308c9c52ba971a6aa16aeee187949b022de
Valerie Thompson <xianthe.xian@gmail.com>
2013-03-07 20:03:09 -0600

    Adding events back

--- js/app.js
+++ js/app.js
@@ -59,14 +59,14 @@ App.TablesRoute = Ember.Route.extend({
 // App.TableController = Ember.ObjectController.extend();
 
 App.FoodController = Ember.ArrayController.extend({
-  // addFood: function(food){
-  //   var table = this.controllerFor('table').get('model'),
-  //     tabItems = table.get('tab.tabItems');
-  //   tabItems.createRecord({
-  //     food: food,
-  //     cents: food.get('cents')
-  //   });
-  // }
+  addFood: function(food){
+    var table = this.controllerFor('table').get('model'),
+      tabItems = table.get('tab.tabItems');
+    tabItems.createRecord({
+      food: food,
+      cents: food.get('cents')
+    });
+  }
 });
 
 //Ember will generate Delete
